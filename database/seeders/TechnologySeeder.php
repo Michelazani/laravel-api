fc57c828ae2da9bcc45cffbaf4477d47de7b1165
Michela Zani <145712045+Michelazani@users.noreply.github.com>
2024-02-20 15:59:46 +0100

    Initial commit

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $technologies=[
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Vue',
+            'PHP', 
+            'Laravel'
+        ];
+        
+        foreach ($technologies as $singleTechnology) {
+            $newTechnology = new Technology();     
+            $newTechnology -> technology = $singleTechnology;
+            $newTechnology->save();
+
+        }
+    }
+}
